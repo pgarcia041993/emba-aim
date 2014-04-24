52d5ea3843a3688213808c2086e05e2dd770d3fb
Patrick Garcia <pgarcia041993@gmail.com>
2014-04-24 15:27:42 +0800

    landing page for emba

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+EmbaAim::Application.config.secret_key_base = 'c98234a3ddb69769878ec2562cddc1daf553f3a7ef10db87177d2021431d833918ac97bff99e68b720b8781962d11150eff04c4047581ce690d414391306ebdb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+EmbaAim::Application.config.session_store :cookie_store, key: '_emba-aim_session'
